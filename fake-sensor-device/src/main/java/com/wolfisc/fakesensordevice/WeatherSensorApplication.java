f6d276117d51e0ed06f36f5d3923b38a74d61b51
jean <jean18699@gmail.com>
2021-11-03 18:15:50 -0400

    servidor activemq implementado en el main

--- fake-sensor-device/src/main/java/com/wolfisc/fakesensordevice/WeatherSensorApplication.java
+++ fake-sensor-device/src/main/java/com/wolfisc/fakesensordevice/WeatherSensorApplication.java
@@ -8,15 +8,18 @@ import com.wolfisc.fakesensordevice.deviceinfo.DeviceInfoGenerator;
 import com.wolfisc.fakesensordevice.deviceinfo.DeviceSenderTask;
 import com.wolfisc.fakesensordevice.jms.JmsProducer;
 import com.wolfisc.fakesensordevice.jms.JmsProducerImpl;
+import com.wolfisc.fakesensordevice.jms.QueueType;
+import org.apache.activemq.ActiveMQConnectionFactory;
 
+import javax.jms.*;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class WeatherSensorApplication {
 
-    public static void main(String[] args) throws IOException {
-        String brokenUrl = Environment.get(Environment.BROKEN_URL, String.class);
+    public static void main(String[] args) throws IOException, JMSException {
+        String brokenUrl = Environment.get(Environment.BROKER_URL, String.class);
         String jmsUser = Environment.get(Environment.JMS_USER, String.class);
         String jmsPass = Environment.get(Environment.JMS_PASS, String.class);
         String queueName = Environment.get(Environment.QUEUE_NAME, String.class);
@@ -32,6 +35,37 @@ public class WeatherSensorApplication {
         DeviceInfoGenerator deviceInfoGenerator = new DeviceInfoGenerator(faker);
         JmsProducer producer = new JmsProducerImpl(brokenUrl, jmsUser, jmsPass);
 
+        //Creating the connection
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokenUrl);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        //Creating the session
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+
+        //Creating the destination
+        Destination destination = session.createTopic("sensors");
+
+        // Create a MessageProducer from the Session to the Topic or Queue
+        MessageProducer producer1 = session.createProducer(destination);
+        producer1.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        //Create the consumers
+        MessageConsumer consumer = session.createConsumer(destination);
+
+        //Creating the message
+        TextMessage message = session.createTextMessage("funcionando correctamente");
+
+        //Tell the producer to send the message
+        producer1.send(message);
+
+        
+        //Clean up
+        session.close();
+        connection.close();
+
+
+
         Timer sendDeviceInfoJob = new Timer();
         TimerTask sender = new DeviceSenderTask(deviceInfoGenerator, objectMapper, queueName, producer);
         sendDeviceInfoJob.scheduleAtFixedRate(sender, sendEveryMs, sendEveryMs);
@@ -40,5 +74,8 @@ public class WeatherSensorApplication {
             sendDeviceInfoJob.cancel();
             sendDeviceInfoJob.purge();
         }));
+
+
+
     }
 }
